f2b6e545f0686ba39e018b0dbcb1e1dc7477e8e7
able <1908561871@qq.com>
2015-07-23 18:35:30 +0800

    代码提交

--- app/src/main/java/zwb/cn/mvp/view/SplashActivity.java
+++ app/src/main/java/zwb/cn/mvp/view/SplashActivity.java
@@ -1,17 +1,35 @@
 package zwb.cn.mvp.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import zwb.cn.demo.Demo;
 import zwb.cn.social.R;
 
 
 public class SplashActivity extends ActionBarActivity {
 
+    @Bind(R.id.bt_action)
+    Button bt_action;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.bt_action)
+    public void click(){
+        Intent intent =new Intent(this, Demo.class);
+        startActivity(intent);
+
+    }
+
+
 
 }
