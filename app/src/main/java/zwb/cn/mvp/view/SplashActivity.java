786fe37fbbfec55b586a9cbac6d8bf827b06f90b
1908561871@qq.com <1908561871@qq.com>
2015-07-31 01:13:11 +0800

    添加jar包

--- app/src/main/java/zwb/cn/mvp/view/SplashActivity.java
+++ app/src/main/java/zwb/cn/mvp/view/SplashActivity.java
@@ -1,17 +1,34 @@
 package zwb.cn.mvp.view;
 
 import android.os.Bundle;
+import android.widget.Button;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.greenrobot.event.EventBus;
 import zwb.cn.app.BaseActivity;
+import zwb.cn.im.manager.IMCoreHelper;
 import zwb.cn.social.R;
+import zwb.cn.view.eidttext.MaterialEditText;
 
 
 public class SplashActivity extends BaseActivity {
-
-
+   @Bind(R.id.et_name)
+    MaterialEditText et_name;
+    @Bind(R.id.bt_login)
+    Button bt_login;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.bt_login)
+    public void click(){
+        IMCoreHelper.login(et_name.getText().toString(),this);
+
+    }
+
 }
